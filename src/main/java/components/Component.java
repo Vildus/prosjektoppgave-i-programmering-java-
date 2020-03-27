bc3ff2e60a02b978bb751a1cc36255ff3d339be9
Chonlawit <chonlawit.wp@hotmail.com>
2020-03-27 19:28:46 +0100

    lagt til getter/setter

--- src/main/java/components/Component.java
+++ src/main/java/components/Component.java
@@ -9,4 +9,20 @@ public class Component {
         this.price = price;
         this.type = type;
     }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
