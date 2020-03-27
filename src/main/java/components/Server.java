7627bb81867d05427c48d4dd4e72a709c0a6116d
Chonlawit <chonlawit.wp@hotmail.com>
2020-03-27 17:31:49 +0100

    lagt til en integer class

--- src/main/java/components/Server.java
+++ src/main/java/components/Server.java
@@ -1,8 +1,14 @@
 package components;
 
 public class Server {
+    private int tall;
+
     public void test(){
         System.out.println("test, test");
+    }
+
+    public int integer(int wdwd){
+        this.tall = wdwd;
     }
 
 }
