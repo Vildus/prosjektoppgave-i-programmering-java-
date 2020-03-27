19888294717723559479063ca5a1e5bb7787d42a
Vildus <56063879+Vildus@users.noreply.github.com>
2020-03-27 17:25:18 +0100

    Slettet alt (#2)

--- src/main/java/components/Server.java
+++ src/main/java/components/Server.java
@@ -1,28 +1,4 @@
 package components;
 
 public class Server {
-
-    int price;
-    String name;
-
-    public Server(int price, String name) {
-        this.price = price;
-        this.name = name;
-    }
-    public void hei(){
-        System.out.println("Hei vilde");
-    }
-
-    public int getPrice(int i) {
-        return i; //hei om dette er riktig
-    }
-
-    public void morradi() {
-        System.out.println("er kul as ploppsann");
-    }
-
-    public void heipådeg() {
-        //gjør noe her
-    }
-
 }
