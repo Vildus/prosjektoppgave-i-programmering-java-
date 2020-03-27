9bcc2d86599e0708ef27029c3c624b558c96e2a2
Vilde Fauchald <vilde.fauchald@gmail.com>
2020-03-27 15:31:29 +0100

    Testmappe lagt til

---
+++ src/test/main/java/files/FileSaverTest.java
@@ -0,0 +1,14 @@
+package files;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileSaverTest {
+
+    @Test
+    public void testFile() {
+
+    }
+
+}
