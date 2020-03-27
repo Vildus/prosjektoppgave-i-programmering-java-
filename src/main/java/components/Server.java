d7e6d64baec01f0e0069b1ce77631a97f24332ae
Vilde Fauchald <vilde.fauchald@gmail.com>
2020-03-27 15:09:56 +0100

    Mappestruktur lagt til og en klasse

---
+++ src/main/java/components/Server.java
@@ -0,0 +1,12 @@
+package components;
+
+public class Server {
+
+    int price;
+    String name;
+
+    public Server(int price, String name) {
+        this.price = price;
+        this.name = name;
+    }
+}
