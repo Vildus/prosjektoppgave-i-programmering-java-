c1e0683d38fc6a0e502fded7236ef472e59f854c
Vilde Fauchald <vilde.fauchald@gmail.com>
2020-03-27 18:43:31 +0100

    Nå starter vi!

---
+++ src/main/java/components/Component.java
@@ -0,0 +1,12 @@
+package components;
+
+public class Component {
+    private double price;
+    private String type;
+
+
+    public Component(double price, String type) {
+        this.price = price;
+        this.type = type;
+    }
+}
