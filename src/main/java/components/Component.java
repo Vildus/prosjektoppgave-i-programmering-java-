25bcf54caca0091b2b20bd6d0bc4a04d300a8c32
Bozhol <57220912+Bozhol@users.noreply.github.com>
2020-04-01 14:22:09 +0200

    Jeg lagde en metode for noe som chon sa

--- src/main/java/components/Component.java
+++ src/main/java/components/Component.java
@@ -3,10 +3,20 @@ package components;
 public class Component {
     private double price;
     private String type;
+    private String Beskrivelse;
 
 
-    public Component(double price, String type) {
+    public Component(double price, String type, String Beskrivelse) {
         this.price = price;
         this.type = type;
+        this.Beskrivelse = Beskrivelse;
+    }
+
+
+    public String leggTil(){
+
+
+        return "Dette går ikke" ;
+
     }
 }
