e9834c2cdf5f8786c9bf225e6ff155e349d66220
Chonlawit <chonlawit.wp@hotmail.com>
2020-04-03 12:30:07 +0200

    Opprettet FileOpenObj og InterfaceFileOpen

---
+++ src/main/java/filehandling/FileOpenObj.java
@@ -0,0 +1,18 @@
+package filehandling;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class FileOpenObj implements InterfaceFileOpen {
+    @Override
+    public List<?> readFile(Path filePath) throws IOException, ClassNotFoundException {
+        InputStream in = Files.newInputStream(filePath);
+        ObjectInputStream oin = new ObjectInputStream(in);
+        Object component = oin.readObject();
+        return (List<?>) component;
+    }
+}
