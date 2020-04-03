b9a69f00bb19a2d31a270e993b8c1b9133d66095
Vilde Fauchald <vilde.fauchald@gmail.com>
2020-04-03 12:46:16 +0200

    La til get- og set-metoder i Component

--- src/main/java/components/Component.java
+++ src/main/java/components/Component.java
@@ -17,4 +17,35 @@ public abstract class Component {
 
     public abstract String getCategory();
 
+    public String getBrand() {
+        return this.brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getArticleNumber() {
+        return this.articleNumber;
+    }
+
+    public void setArticleNumber(int articleNumber) {
+        this.articleNumber = articleNumber;
+    }
 }
