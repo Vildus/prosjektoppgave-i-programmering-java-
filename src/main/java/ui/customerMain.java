ecdc1a3cbab179eae5003ddf1ea11a9cf769db91
chonlawit93 <60465274+chonlawit93@users.noreply.github.com>
2020-04-16 22:58:50 +0200

    Sluttbruker gui (#29)
    * la Customer fxml,Controller,Main,css. Men får ikke til å kjøre den (Invalide source realease: 11)
    
    * pushet alt fra customer

---
+++ src/main/java/ui/customerMain.java
@@ -0,0 +1,20 @@
+package ui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class customerMain extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("customer.fxml"));
+        primaryStage.setTitle("Test");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
