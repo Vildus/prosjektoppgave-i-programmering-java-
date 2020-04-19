fc43f7ce90b1137d2249511c004518a79405e402
Vildus <56063879+Vildus@users.noreply.github.com>
2020-04-19 14:12:28 +0200

    Inventory controller (#31)
    * Endret til isEmpty foreløpig men må finne en annen løsning for å sjekke om strengen er helt tom
    
    * lagt til metoder på item for å få tak i getComponentBrand og getComponentModel. Disse var kun tilgjengelige fra component.
    
    * inventoryController, fxml-fil oppdatert. Har også lagt til nye klasse for å kunne søke i ui. Ny klasse: FilerTableViewItemPredicate. Måtte også legge til en metode på inventory
    
    * Har lagt til en ekstra konstruktør samt endret navn på metoder i inventoryRepostory
    
    * Lagt til css-fil for å style inventory-scene. Ikke gjort så mye enda
    
    * Lagt til combobox i inventoryController
    
    * endringer

--- src/main/java/io/InventoryRepository.java
+++ src/main/java/io/InventoryRepository.java
@@ -1,23 +1,39 @@
 package io;
 
 import inventory.Inventory;
+
 import java.io.*;
+import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 
 public class InventoryRepository {
     private Path directory;
 
+    //bruk denne konstruktøren i testing når vi vil spesifieserer den
     public InventoryRepository(Path directory) {
         this.directory = directory;
     }
 
-    private Path getInventoryPath() {
-        return Path.of(this.directory.toString(), "inventory.jobj");
+    // denne konstruktøren skal brukes i ui fordi path er predefinert/default
+    public InventoryRepository() throws IOException {
+        String cwd = System.getProperty("user.dir");
+        Path storePath = Paths.get(cwd, "datastore");
+        try {
+            Files.createDirectory(storePath);
+        } catch (FileAlreadyExistsException e) {
+            // This is ok - datastore dir already created
+        }
+        this.directory = storePath;
     }
 
-    public void saveInventory(Inventory inventory) throws IOException {
+    private Path getInventoryPath() {
+        return Paths.get(this.directory.toString(), "inventory.jobj");
+    }
+
+    public void save(Inventory inventory) throws IOException {
         Path path = this.getInventoryPath();
         OutputStream os = Files.newOutputStream(path);
         ObjectOutputStream out = new ObjectOutputStream(os);
@@ -26,8 +42,11 @@ public class InventoryRepository {
         out.close();
     }
 
-    public Inventory ReadInventory() throws IOException, ClassNotFoundException {
+    public Inventory read() throws IOException, ClassNotFoundException {
         Path path = this.getInventoryPath();
+        if (Files.exists(path) == false) {
+            throw new FileNotFoundException(path.toString());
+        }
         InputStream in = Files.newInputStream(path);
         ObjectInputStream oin = new ObjectInputStream(in);
         Object inventory = oin.readObject();
