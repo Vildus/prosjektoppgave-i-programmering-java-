972fc7436b8723f5a6e649769bcdf77cb9b9c12a
Vilde Fauchald <vilde.fauchald@gmail.com>
2020-05-11 13:42:31 +0200

    OrderHistoryController

--- src/main/java/ui/OrderHistoryController.java
+++ src/main/java/ui/OrderHistoryController.java
@@ -1,4 +1,42 @@
 package ui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import purchase.Order;
+
+import java.util.Date;
+
+
 public class OrderHistoryController {
+
+
+    @FXML
+    private Button btnClose;
+
+    @FXML
+    private TableView<Order> tvOrderHistory;
+
+    @FXML
+    private TableColumn<Order, Integer> colOrderNumber;
+
+    @FXML
+    private TableColumn<Order, Date> colDate;
+
+    @FXML
+    private TableColumn<Order, Double> colTotalPrice;
+
+    @FXML
+    private Label lblCustomerID;
+
+    @FXML
+    void handleClose(ActionEvent event) {
+
+    }
+
 }
+
+
