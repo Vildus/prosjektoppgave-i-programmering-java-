5a20e43cd6dc226a9aa380d06e4c33adac0c7d01
Vilde Fauchald <vilde.fauchald@gmail.com>
2020-05-13 10:41:01 +0200

    Setter qty-feltet tilbake til en etter redigering

--- src/main/java/ui/CustomerController.java
+++ src/main/java/ui/CustomerController.java
@@ -162,6 +162,7 @@ public class CustomerController {
                                 int parseQty = Integer.parseInt(txtQty.getText());
                                 ShoppingBag.getInstance().addItem(new ShoppingBagItem(item, parseQty));
                                 Alert.showInfoDialog("Added to shoppingbag", String.format("%d pcs of articlenumber: %d is added to shopping bag", parseQty, item.getArticleNumber()));
+                                this.txtQty.setText("1");
                             } catch (ItemAvailableStockException e) {
                                 Alert.showInfoDialog("Out of stock", "We are sorry, this item is out of stock", e);
                             } catch (NumberFormatException e) {
