c388d462d4c1127396bc1028a0cd5ef7dea0bc9f
Vilde Fauchald <vilde.fauchald@gmail.com>
2020-05-13 12:27:28 +0200

    endret fra label til textbox fordi vi trenger scrolling. Har også lagt til DATE og orderNumber i utskriften

--- src/main/java/ui/OrderConfirmationController.java
+++ src/main/java/ui/OrderConfirmationController.java
@@ -2,27 +2,31 @@ package ui;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import purchase.Customer;
+import purchase.Order;
 import purchase.ShoppingBag;
 import purchase.ShoppingBagItem;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class OrderConfirmationController {
 
 
     public static final String TITLE = "Order confirmation";
 
+    private Order order;
 
     private SceneCloser sceneCloser;
 
-    public OrderConfirmationController(SceneCloser sceneCloser) {
+    public OrderConfirmationController(Order order, SceneCloser sceneCloser) {
+        this.order = order;
         this.sceneCloser = sceneCloser;
     }
 
     @FXML
-    private Label lblOrderInfo;
+    private TextArea txtOrderInfo;
 
 
     @FXML
@@ -37,7 +41,7 @@ public class OrderConfirmationController {
 
     private void printShoppingBag() {
 
-//TODO: Order date, Customer ID
+//TODO: Order date, Order number
         ArrayList<String> lines = new ArrayList<>();
 
         for (ShoppingBagItem shoppingBagItem : ShoppingBag.getInstance().getShoppingBagItems()) {
@@ -50,10 +54,12 @@ public class OrderConfirmationController {
         }
 
         String customerID = Customer.getCurrentCustomerID();
+        int orderNumber = this.order.getOrderNumber();
+        Date date = this.order.getDate();
         String output = "";
         output = output + String.join("\n", lines) +
                 String.format("\n\n\nTotal order price: %.2f NOK", ShoppingBag.getInstance().getTotalPrice());
-        this.lblOrderInfo.setText(String.format("Customer ID: %s\n\nOrder info:\n\n%s", customerID, output));
+        this.txtOrderInfo.setText(String.format("Customer ID: %s\n\nOrder number: %d\nDate: %s\n\nOrder info:\n\n%s", customerID, orderNumber, date, output));
     }
 
 
